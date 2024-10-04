66dce1a426eec3aaeca9791ce0f9860dcc33e3d7
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-10-04 19:54:17 +0200

    Refactor routing in App component to add new pages for Admin, Agent, and Farmer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
 import Login from './Pages/Login/Login'
 import Dashboard from './Pages/Dashboard/Dashboard'
+import Admin from './Pages/Admin/Admin'
+import Agent from './Pages/Agent/Agent'
+import Farmer from './Pages/Farmer/Farmer'
 
 function App() {
   return (
@@ -10,6 +11,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<Admin />} />
+        
+        <Route path="/agent" element={<Agent />} />
+        <Route path="/farmer" element={<Farmer />} />
+
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Router>
   );
